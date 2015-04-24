fc5e08c25fbdb4fe94102cea8a83af782255e608
Dominik Rosiek <45dominik@gmail.com>
2015-04-24 13:18:05 +0200

    fb war

--- app/src/main/java/slowhackaton/com/findmetouchmecatchme/MainActivity.java
+++ app/src/main/java/slowhackaton/com/findmetouchmecatchme/MainActivity.java
@@ -5,13 +5,17 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 
@@ -23,11 +27,34 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("001", "start");
         that=this;
         FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_main);
         callbackManager = CallbackManager.Factory.create();
-        startbutton = (LoginButton)findViewById(R.id.start_button);
+        startbutton = (LoginButton)findViewById(R.id.login_button);
+
+        startbutton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                Log.d("tunak, tunak tun", "zalogowany :)");
+                Intent startRangingaAndDisplaying = new Intent(that, rangingAndDisplaying.class);
+                startActivity(startRangingaAndDisplaying);
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("tunak, tunak tun", "cancel");
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.d("tunak, tunak tun", "error");
+                // App code
+            }
+        });
         startbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
